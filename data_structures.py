d99a159f191e1dfd55357691ed78b64b74974bc0
Enock Otieno <otienoenock3@gmail.com>
2025-04-01 08:08:19 +0300

    Completed assignment

--- data_structures.py
+++ data_structures.py
@@ -1,7 +1,5 @@
-# creating an empty list. 
+# create an empty list. Append elements to my_list
 my_list = []
-
-# appending elements to my_list
 
 my_list. append(10)
 my_list. append(20)
@@ -17,7 +15,7 @@ my_list.insert(1, 15)
 new_list = [50, 60, 70]
 my_list.extend(new_list)
 
-#removing last element, sorting ascending, and print index of 30. 
+#remove last element, sort ascending, and print index of 30. 
 my_list.pop()
 my_list.sort(reverse = False)
 print(my_list.index(30))
